5bfc6c414d23aae9707e4b493089504ecb5852e0
dugidugi <duggis92@gmail.com>
2022-03-17 21:44:23 +0900

    Storing session in DB

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
@@ -16,19 +17,14 @@ app.set("views", process.cwd() + "/src/views/");
 app.use(
     session({
         secret: "hello!",
-        resave: true,
-        saveUninitialized: true,
+        store: MongoStore.create({ mongoUrl: 'mongodb://127.0.0.1:27017/wetube'}),
+        resave: false,
+        saveUninitialized: false,
     })
 );
 
-app.use((req, res, next) => {
-    req.sessionStore.all((error, sessions) => {
-        console.log(sessions);
-        next();
-    });
-});
-
 app.use(localsMiddleware);
+
 app.use(logger);
 app.use(express.urlencoded({extended: true}));
 app.use("/", rootRouter);
