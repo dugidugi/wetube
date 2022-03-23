eeaf325fd04ff83b62e049584b2afdb64c1f12f3
dugidugi <duggis92@gmail.com>
2022-03-23 22:43:48 +0900

    videoRouter에 프로텍터 미들웨어

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -11,9 +11,9 @@ import {
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
-videoRouter.get("/:id([0-9a-f]{24})/delete", deleteVideo);
-videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter.all(protectorMiddleware).get("/:id([0-9a-f]{24})/delete", deleteVideo);
+videoRouter.all(protectorMiddleware).route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.all(protectorMiddleware).route("/upload").get(getUpload).post(postUpload);
 
 
 export default videoRouter;
