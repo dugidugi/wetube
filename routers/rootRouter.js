d46980d12f64a07f94864ad458989e9824734d2b
dugidugi <duggis92@gmail.com>
2022-03-23 22:41:23 +0900

    protectorMiddleware, publicOnlyMiddleware 처리

--- routers/rootRouter.js
+++ routers/rootRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { getJoin, getLogin, logout, postJoin, postLogin } from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
+import { publicOnlyMiddleware } from "../middlewares";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", home);
-globalRouter.route("/join").get(getJoin).post(postJoin);
-globalRouter.route("/login").get(getLogin).post(postLogin);
+globalRouter.route("/join").all(publicOnlyMiddleware).get(getJoin).post(postJoin);
+globalRouter.route("/login").all(publicOnlyMiddleware).get(getLogin).post(postLogin);
 globalRouter.get("/logout", logout);
 globalRouter.get("/search", search);
 
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { edit, remove, logout, see, startGithubLogin, finishGithubLogin, getEdit, postEdit } from "../controllers/userController";
+import { protectorMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.route("/edit").get(getEdit).post(postEdit);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
 userRouter.get("/remove", remove);
 userRouter.get(":id", see);
 userRouter.get("/github/start", startGithubLogin);
